5dd272b837343d519e0857cb9518aff6881e09d8
JoaogPimentel <joaog.pimentelst@gmail.com>
2024-10-10 09:11:59 -0300

    added option for user to change  password

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.core.exceptions import ValidationError
+from django.contrib.auth.hashers import check_password
 from .models import UserProfile, OFFICE_STATUS
 from stock.models import Sector, Unit
 
@@ -47,16 +49,54 @@ class CustomUserCreationForm(UserCreationForm):
 class CustomUserChangeForm(UserChangeForm):
     
     profile_image = forms.ImageField(required=False)
+    current_password = forms.CharField(
+        label='Senha Atual',
+        widget=forms.PasswordInput(attrs={'autocomplete':'new-password'}),
+        required=False,
+    )
+    new_password = forms.CharField(
+        label='Nova Senha',
+        widget=forms.PasswordInput,
+        required=False,
+    )
+    confirm_password = forms.CharField(
+        label='Confirmar Nova Senha',
+        widget=forms.PasswordInput,
+        required=False,
+    )
     
     class Meta:
         model = User
         fields = ("profile_image", "username", "email", "first_name", "last_name",)
 
+    def clean_current_password(self):
+        current_password = self.cleaned_data.get('current_password')
+        if current_password:
+            if not check_password(current_password, self.instance.password):
+                raise ValidationError("A senha atual está incorreta.")
+        return current_password
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password = cleaned_data.get('new_password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if new_password or confirm_password:
+            if new_password != confirm_password:
+                raise ValidationError('As novas senhas não coincidem.')
+            
+        return cleaned_data
+
+
     def save(self, commit=True):
         user = super(CustomUserChangeForm, self).save(commit=False)
         user.first_name = self.cleaned_data["first_name"]
         user.last_name = self.cleaned_data["last_name"]
-        
+
+        new_password = self.cleaned_data.get('new_password')
+        if new_password:
+            user.set_password(new_password)
+                    
         if commit:
             user.save()
             
