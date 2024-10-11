0efd758da3ca423c192c8d196abd34f3e48868bf
JoaogPimentel <joaog.pimentelst@gmail.com>
2024-10-11 11:45:00 -0300

    changed the 031 financial migration

--- financial/migrations/0031_rename_financialclasification_subcategory_and_more.py
+++ financial/migrations/0031_rename_financialclasification_subcategory_and_more.py
@@ -22,6 +22,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='chartofaccounts',
             name='subcategory',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='subcategory_accounts', to='financial.subcategory'),
+            field=models.ForeignKey( null=True, on_delete=django.db.models.deletion.PROTECT, related_name='subcategory_accounts', to='financial.subcategory'),
         ),
     ]
