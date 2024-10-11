6187e9617713dc8bc8b467bc8f3998ff94aad351
JoaogPimentel <joaog.pimentelst@gmail.com>
2024-10-11 11:46:36 -0300

    changed the 035 financial migration

--- financial/migrations/0035_chartofaccounts_category_and_more.py
+++ financial/migrations/0035_chartofaccounts_category_and_more.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='chartofaccounts',
             name='category',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='category_accounts', to='financial.financialcategory'),
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='category_accounts', to='financial.financialcategory'),
             preserve_default=False,
         ),
         migrations.AlterField(
